5eb0b664c86f7d92a126af240f39cf6a0aa40bac
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-01-28 21:54:20 -0300

    Trabalhos com array .pop .push .shift .unshift .splice

--- Array/arrayMetodos.js
+++ Array/arrayMetodos.js
@@ -3,3 +3,28 @@ const pilotos = ['Vertel', 'Alonso', 'Raikkonen' , 'Massa'];
 
 //.pop exclui o ultimo elemento do array.
 pilotos.pop();
+console.log(pilotos);
+// ? [ 'Vertel', 'Alonso', 'Raikkonen' ]
+
+pilotos.push('Vestappen');
+console.log(pilotos);
+// ? [ 'Vertel', 'Alonso', 'Raikkonen', 'Vestappen' ]
+
+
+// .shift remove o primeiro elemento.
+pilotos.shift();
+console.log(pilotos);
+// ? [ 'Alonso', 'Raikkonen', 'Vestappen' ]
+
+
+// .unshift adiciona o elemento no inicio do array.
+pilotos.unshift('Hamilton');
+console.log(pilotos);
+// ?  [ 'Hamilton', 'Alonso', 'Raikkonen', 'Vestappen' ]
+
+/*.splice pode adicionar e remover  elementos
+ ("antes do indice marcado", "numerode elementos afetados", "elementos adicionados") */
+ pilotos.splice(2,0,'Bottas', 'Massa');
+ console.log(pilotos);
+ // ?
+
