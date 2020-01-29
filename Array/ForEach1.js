34b2677eddeb2b008404a6df327d6cbcad920691
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-01-29 10:48:02 -0300

    Exercicios com Foreach

--- Array/ForEach1.js
+++ Array/ForEach1.js
@@ -1,6 +1,26 @@
 //Formas diferentes para pecorrer -->1° Foreach.
 //Sem muitos comnentatios de agora em diante...
-const aprovados = ['Agatha', 'Aldo', 'Daniel','Raquel']
+
+const aprovados = ['Agatha', 'Aldo', 'Daniel','Raquel']// Retorna os nomes e indices.
 aprovados.forEach((nome,indice) => {
-    console.log(`(${indice + 1})--> ${nome}`)
+    console.log(`${indice + 1})  ${nome}`)
 });
+
+console.log('---');
+
+aprovados.forEach((nome, indice, array) => {
+    console.log(array[indice]);
+    
+});
+
+console.log('---');
+
+aprovados.forEach(nome =>{ // Retorna os nomes.
+   console.log(nome);
+});
+
+console.log('---');
+
+const exibirAprovados = aprovado => console.log(aprovado);
+
+aprovados.forEach(exibirAprovados);
