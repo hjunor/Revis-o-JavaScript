bffb4cf5f8569f04ee000bbcef17bd08c62e2fcd
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-01-29 10:18:36 -0300

    Exercicios com Foreach

--- Array/ForEach1.js
+++ Array/ForEach1.js
@@ -1,5 +1,6 @@
-//Formas diferentes para pecorrer -->1° Foreach
-
-array.forEach(element => {
-    
+//Formas diferentes para pecorrer -->1° Foreach.
+//Sem muitos comnentatios de agora em diante...
+const aprovados = ['Agatha', 'Aldo', 'Daniel','Raquel']
+aprovados.forEach((nome,indice) => {
+    console.log(`(${indice + 1})--> ${nome}`)
 });
