c3a1ce224e20ff28d6801e566ce017b77a89af44
Heberth Junor Teixeira Aguiar <hjunor@gmail.com>
2020-01-31 10:01:25 -0300

    Declarativo x imperativo

--- imperativoXdeclarativo/iXp.js
+++ imperativoXdeclarativo/iXp.js
@@ -7,15 +7,14 @@ const alunos = [
 
 
 let total = 0;
-
 for (let i = 0; i < alunos.length; i++){
     total += alunos[i].notas
 }
-
 console.log(total / alunos.length); // retorna a media dos alunos.
 
 //declarativo
 
-const total2 = alunos.map(e => e.notas).reduce((a,e) => (a+e)/alunos.length); // retorna a media dos alunos.
+const total2 = alunos.map(e => e.notas)
+.reduce((a,e,indice, array) => (a+e)/array.length); // retorna a media dos alunos.
 
 console.log(total2);
